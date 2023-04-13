ba16a4ade43f9e2268d692524b8d5072d8a8fd64
moises <angular.moises82@gmail.com>
2023-04-13 04:52:58 -0300

    cambiando el enlace en el environment de desarrollo, poniendo ahi el de produccion para que netfily lo tome

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  url_endpoint : "http://localhost:3000"
+  url_endpoint : "https://api-rest-full-andromeda.onrender.com"
+  //url_endpoint : "http://localhost:3000"
 };
 
 /*
